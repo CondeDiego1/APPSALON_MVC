fb59c2aa9b5b1ecd9d219d5bb7676ffc6fbadb95
DiegoCondeLopez <diego.conde@pascualbravo.edu.co>
2022-05-26 10:29:50 -0500

    Pruebas

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -145,6 +145,7 @@ class LoginController{
 
     public static function Confirm(Router $router){
         $errores = [];
+        Debuguear($_GET);
         $t = $_GET['token'] ?? 0;
         $token = Sanitizar($t);
         $usuarios = Usuario::ConsultaParametro('token', $token);
