8305134402ec8a52637c0fd990ce28cdb214d5fd
DiegoCondeLopez <diego.conde@pascualbravo.edu.co>
2022-05-26 11:06:06 -0500

    Pruebas

--- includes/database.php
+++ includes/database.php
@@ -1,10 +1,10 @@
 <?php
 
 declare(strict_types=1);
-function conexionBD(): mysqli
+function conexionBD()
 {
     //$db = new mysqli('localhost', 'root', '', 'appsalon');
-    $db = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_DATABASE']);
+    $db = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_DATABASE']);
     $db->set_charset('utf8');
 
     if (!$db) {
