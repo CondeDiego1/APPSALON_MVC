12151d1ea20e1111fd4235b2e9f692709bd933ed
DiegoCondeLopez <diego.conde@pascualbravo.edu.co>
2022-05-28 00:41:10 -0500

    Mejorando estilos

--- models/Email.php
+++ models/Email.php
@@ -41,62 +41,18 @@ class Email{
             <html>
             <style>
             @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap');
-            h2 {
-                font-size: 25px;
-                font-weight: 500;
-                line-height: 25px;
-            }
-        
-            body {
-                font-family: 'Poppins', sans-serif;
-                background-color: #ffffff;
-                width: 400px;
-                max-width: 400px;
-                margin: 0 auto;
-                padding: 20px;
-            }
-        
-            p {
-                line-height: 18px;
-            }
-        
-            a {
-                position: relative;
-                z-index: 0;
-                display: inline-block;
-                margin: 20px 0;
-            }
-        
-            a button {
-                padding: 10px 20px;
-                font-size: 16px;
-                font-weight: 500;
-                background-color: #000000;
-                color: #ffffff;
-                border: none;
-                text-transform: uppercase;
-                cursor: pointer;
-            }
-        
-            p span {
-                font-size: 12px;
-            }
-        
-            div p {
-                border-bottom: 1px solid #000000;
-                border-top: none;
-                margin-top: 40px;
-            }
             </style>
-            <body>
+
+            <body style='width:400px; max-width:400px; margin:0 auto; font-family:Poppins,sans-serif; padding: 20px'>
                 <h1>BarberShop</h1>
-                <h2>¡Gracias por registrarte!</h2>
-                <p>Por favor confirma tu correo electrónico para que puedas comenzar a disfrutar de todos los servicios de
-                    BarberShop</p>
-                <a href='https://enigmatic-coast-02693.herokuapp.com/confirm?token=" . $this->token . "'><button>Verificar</button></a>
-                <p>Si tú no te registraste en BarberShop, por favor ignora este correo electrónico.</p>
-                <div><p></p></div>
-                <p><span>Este correo electrónico fue enviado desde una dirección solamente de notificaciones que no puede aceptar correo electrónico entrante. Por favor no respondas a este mensaje.</span></p>
+                <h2 font-size='25px' font-weight='500' line-height='25px'>¡Gracias por registrarte!</h2>
+                <p style='line-height:18px;'>Por favor confirma tu correo electrónico para que puedas comenzar a disfrutar de todos los servicios de BarberShop</p>
+                <a style='position: relative; z-index: 0; display: inline-block; margin: 20px 0;' href='https://enigmatic-coast-02693.herokuapp.com/confirm?token=" . $this->token . "'><button style='padding: 10px 20px; font-size: 16px; font-weight: 500; background-color: #000000; color: #ffffff; border: none; text-transform: uppercase; cursor: pointer;'>Verificar</button></a>
+                <p style='line-height:18px;'>Si tú no te registraste en BarberShop, por favor ignora este correo electrónico.</p>
+                <div>
+                    <p style='border-bottom: 1px solid #000000; border-top: none; margin-top: 40px;'></p>
+                </div>
+                <p style='line-height:18px;'><span font-size='12px'>Este correo electrónico fue enviado desde una dirección solamente de notificaciones que no puede aceptar correo electrónico entrante. Por favor no respondas a este mensaje.</span></p>
             </body>
             </html>";
 
