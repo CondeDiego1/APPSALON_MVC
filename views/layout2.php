9237d016e98e278cf108094452bee18d1950caed
DiegoCondeLopez <diego.conde@pascualbravo.edu.co>
2022-05-28 13:02:17 -0500

    Mejorando el frontend

--- views/layout2.php
+++ views/layout2.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=0, width=device-width, height=device-height"/>
     <title>App BarberShop</title>
     <link rel="stylesheet" href="/build/css/app.css">
     <meta name="description" content="Página web de Salon de Belleza">
