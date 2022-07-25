e5b04c1dabe8af79abc85a5c18fb05ad9031ceb0
DiegoCondeLopez <diego.conde@pascualbravo.edu.co>
2022-07-25 15:44:49 -0500

    Eliminando fallos

--- includes/database.php
+++ includes/database.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 function conexionBD()
 {
-    //$db = new mysqli('localhost', 'root', '', 'appsalon');
     $db = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_DATABASE']);
     $db->set_charset('utf8');
 
